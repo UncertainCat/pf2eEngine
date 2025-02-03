f7735ed35bc9dd828d415b2836dffc4b77518bcc
UncertainCat <josh.a.villatoro@gmail.com>
2025-02-03 11:57:39 -0600

    unpack fixes

--- scripts/consolidator.go
+++ scripts/consolidator.go
@@ -1,5 +1,5 @@
 package scripts
 
-const StartDelimiter = "====== AI-PACKER-FILE: %s ======\n"
-const EndDelimiter = "====== AI-PACKER-END: %s ======\n"
+const StartDelimiter = "====== FILE-START: %s ======\n"
+const EndDelimiter = "====== FILE-END: %s ======\n"
 const OutputFileName = "packed_files.txt"
